1965dca578693dca7b36474fe53683892e609b4e
Ethan <ethanhorr@gmail.com>
2018-10-08 13:07:54 +0100

    JSON response

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var app = express();
+
+
 app.get('/', function(req, res) {
-    res.send('Welcome to Data Representation & Querying');
+    res.send('Hello World');
 })
+
+app.get('/hello/:name', function (req, res) {
+    console.log(req.params.name);
+    res.send('Hello ' + req.params.name);
+})
+ 
+ //var jsonResponse = require("response.json"); // Path to response.JSON
+
+ app.get('/api/data', function(req, res){
+    const posts = [
+        {
+            "posts": [
+                {
+                    "id": "fadf12421l",
+                    "title": "First server-side post",
+                    "content": "This is coming from the server"
+                },
+                {
+                    "id": "ksajflaj132",
+                    "title": "Second server-side post",
+                    "content": "This is coming from the server!"
+                }
+            ]
+        }
+    ]
+});
+
 var server = app.listen(8081, function() {
     var host = server.address().address
     var port = server.address().port
+
     console.log("Example app listening at http://%s:%s", host, port)
 })
