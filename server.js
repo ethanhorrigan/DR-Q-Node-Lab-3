b1f259fae24975d171c2781dc66cc21c9ea5b9c4
Ethan <ethanhorr@gmail.com>
2018-10-08 12:32:46 +0100

    Express added

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-var http = require("http");
-
-http.createServer(function(reuqest, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    // Send the response body as "Welcome to Data Representation & Querying"
-    response.end('Welcome to Data Representation & Querying\n');
-
-}).listen(8081);
-
-console.log("Server running @ 127.0.0.1/8081");
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+    res.send('Welcome to Data Representation & Querying');
+})
+var server = app.listen(8081, function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
