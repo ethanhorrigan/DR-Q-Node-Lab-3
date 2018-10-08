87ca0fc902aa04c8f40aab72df2a92599d95fe09
Ethan <ethanhorr@gmail.com>
2018-10-08 12:23:22 +0100

    Added README & Updated server.js

---
+++ README.md
@@ -0,0 +1,6 @@
+What is Express?
+
+Express is a minimal and flexible Node.js web application framework
+that provides a robust set of features to develop web and mobile
+applications. It facilitates the rapid development of Node based
+Web applications.
--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+var http = require("http");
+
+http.createServer(function(reuqest, response) {
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    // Send the response body as "Welcome to Data Representation & Querying"
+    response.end('Welcome to Data Representation & Querying\n');
+
+}).listen(8081);
+
+console.log("Server running @ 127.0.0.1/8081");
